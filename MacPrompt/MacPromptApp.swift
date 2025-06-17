7426d60285f9fb28b413a6d84413ea62f8ef0af9
SCHOOMP <grahamschomp2@gmail.com>
2025-06-17 12:26:19 -0500

    Initial Commit

---
+++ MacPrompt/MacPromptApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPromptApp.swift
+//  MacPrompt
+//
+//  Created by Graham Schomp on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MacPromptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacPromptTests/MacPromptTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacPromptTests.swift
+//  MacPromptTests
+//
+//  Created by Graham Schomp on 6/17/25.
+//
+
+import Testing
+@testable import MacPrompt
+
+struct MacPromptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
